52fcaaea06c3f3266710ef5c24e118eedb886bd4
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-02-16 00:15:35 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,12 +1,13 @@
-if(isset($_POST['id']) && $_POST['id'] != '' )
-{
-    $id     = $_POST['id'];
-    $select = $con->prepare("SELECT * FROM data WHERE id=?");
-    $select->bind_param('s', $id);
-    $select->execute();
-    $result = $select->get_result();
-    while($row = $result->fetch_assoc())
-    {
-        echo $row['column 1'] . "\t" . $row['column 2'] . "\n";
-    }
+$process = $_POST['process'];
+$log = "/logs/logs.txt";
+
+if($process == 'update'){
+    //execute mysqli update command and update table.
+    $str = "Update on " . date('d/m/Y - H:i:s') . "\n";//add some text to the logs file (can be anything just to increase the logs.text size)
+    file_put_content($log, $str, FILE_APPEND);//FILE_APPEND add string to the end of the file instead or replacing it's content
+}
+else if($process == 'insert'){
+    //execute mysqli insert command and add new data to table.
+    $str = "Added new data on" . date('d/m/Y - H:i:s') . "\n";
+    file_put_content($log, $str, FILE_APPEND);
 }
